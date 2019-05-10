2edd5bb8a2b092bea134eb0faad40e4e490c5d34
KennyStu <kenny-lee@familie-studier.de>
2019-05-10 11:31:21 +0200

    zufälliger Teleporterraum /unfertig

--- src/zuul/WorldGenerator.java
+++ src/zuul/WorldGenerator.java
@@ -1,23 +1,52 @@
 package zuul;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
+
 
 public class WorldGenerator {
     private Raum lichtung, waldstueck, taverne, hexenhaus, dorfplatz, kellerDerTaverne, geheimgang, taverneErsterStock,
     				piratenHoehle, teleporter;
     
+    private HashMap<String, Raum> alleRaume;
+    
+    
     public WorldGenerator() {
-        this.raeumeAnlegen();
+    	this.alleRaume = new HashMap<>();
+    	this.raeumeAnlegen();
         this.setzeAusgaenge();
         this.addGegenstaende();
+    }
+    
+    private Raum getZufaelligerRaum() {
+    	Raum zufaelligerRaum = null;
+    	Random r = new Random(alleRaume.size()); // Random r gib eine -... zahl zurück.. ?!*************************
+    	System.out.println(r.nextInt()); // Diese Zeile weg..
+    	zufaelligerRaum = new ArrayList<Raum>(alleRaume.values()).get(r.nextInt());
+    	
+    	return zufaelligerRaum;
     }
     
     //** legt die Räume an mit beschreibung (String) und kälte (int)
     private void raeumeAnlegen() {
     	
-        lichtung = new Raum("auf einer Lichtung, umgeben von dunklen Tannen", 12);
-        waldstueck = new Raum("im dunklen Wald", 13);
+    	this.alleRaume.put("lichtung", new Raum("auf einer Lichtung, umgeben von dunklen Tannen", 12));
+    	this.alleRaume.put("waldstueck", new Raum("im dunklen Wald", 13));
+    	this.alleRaume.put("taverne", new Raum("in der Taverne, mit zwielichten Gestalten an der Theke", 20));
+    	this.alleRaume.put("hexenhaus", new Raum("im Hexenhaus, mit einem größem Symbol auf dem Boden", 18));
+    	this.alleRaume.put("dorfplatz", new Raum("auf dem Dorfplatz", 15));
+    	this.alleRaume.put("piratenHoehle", new Raum("in einer kalten und nassen alten Piratenhöhle", 2));
+    	this.alleRaume.put("kellerDerTaverne", new Raum("im Keller der Taverne", 9));
+    	this.alleRaume.put("geheimgang", new Raum("in einem schmalen modrigen Geheimgang", 7));
+    	this.alleRaume.put("taverneErsterStock", new Raum("bei den den Gästezimmern im ersten Stock der Taverne", 20));
+    	this.alleRaume.put("teleporter", new Raum("in einem kleinem kaltem Raum mit einem größem Symbol auf dem Boden", 3));
+    	
+    	
+  /*     
+    	lichtung = new Raum("auf einer Lichtung, umgeben von dunklen Tannen", 12);
+    	waldstueck = new Raum("im dunklen Wald", 13);
         taverne = new Raum("in der Taverne, mit zwielichten Gestalten an der Theke", 20);
         hexenhaus = new Raum("im Hexenhaus, mit einem größem Symbol auf dem Boden", 18);
         dorfplatz = new Raum("auf dem Dorfplatz", 15);
@@ -26,10 +55,12 @@ public class WorldGenerator {
         geheimgang = new Raum("in einem schmalen modrigen Geheimgang", 7);
         taverneErsterStock=new Raum("bei den den Gästezimmern im ersten Stock der Taverne", 20);
         teleporter = new Raum("in einem kleinem kaltem Raum mit einem größem Symbol auf dem Boden", 3);
+   */
     }
     
 //** die Ausgänge initialisieren/ den Räumen ein befehlswort zum raumwechsel und den ausgang (Raum) zuweisen
     private void setzeAusgaenge() {
+    	Raum zufaelligerRaum = getZufaelligerRaum();
     	
         lichtung.setAusgang("down", piratenHoehle);
         lichtung.setAusgang("east", waldstueck);
@@ -39,7 +70,7 @@ public class WorldGenerator {
         dorfplatz.setAusgang("north", waldstueck);
         dorfplatz.setAusgang("south", taverne);
         hexenhaus.setAusgang("east", dorfplatz);
-        hexenhaus.setAusgang("teleport", teleporter);
+        hexenhaus.setAusgang("teleport", zufaelligerRaum);
         taverne.setAusgang("north", dorfplatz);
         taverne.setAusgang("up", taverneErsterStock);
         taverne.setAusgang("down", kellerDerTaverne);
@@ -53,7 +84,7 @@ public class WorldGenerator {
         piratenHoehle.setAusgang("west", geheimgang);
         piratenHoehle.setAusgang("up", lichtung);
         teleporter.setAusgang("south", geheimgang);
-        teleporter.setAusgang("teleport", hexenhaus);
+        teleporter.setAusgang("teleport", zufaelligerRaum);
 
     }
 
