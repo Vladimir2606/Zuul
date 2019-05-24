5eeed602424f1b2d528be668b62f648645c4a276
Vladimir Elistratov <vladimir27@szut.uni-bremen.de>
2019-05-24 10:11:01 +0200

    rüstung verbessert

--- src/zuul/rüstung/Rüstung.java
+++ src/zuul/rüstung/Rüstung.java
@@ -3,14 +3,14 @@ package zuul.rüstung;
 import zuul.items.Gegenstand;
 
 public class Rüstung extends Gegenstand {
-
+	
 	private int ruestungspunkte;
-
+	
 	public Rüstung(String name, String beschreibung, int gewicht, int ruestungspunkte) {
 		super(name, beschreibung, gewicht);
 		this.ruestungspunkte = ruestungspunkte;
 	}
-
+	
 	public int getRuestung() {
 		return this.ruestungspunkte;
 	}
